27a95ce919a381c7df9d217906d7eef258fead3d
Dmitry Igrishin <dmitigr@gmail.com>
2025-09-17 13:38:27 +0300

    Refactor

--- src/dmitigr_libev.cmake
+++ src/dmitigr_libev.cmake
@@ -1,18 +1,36 @@
 # -*- cmake -*-
 # Copyright (C) Dmitry Igrishin
 
-project(libev)
-cmake_minimum_required(VERSION 3.16)
+# @brief Adds libev with add_library() to the project.
+#
+# @details The output of this macro depends on the following variables:
+#   - `LIBEV_INSTALL` - Boolean which indicates the request to install.
+#
+# @param libev_src_root A path where to look for libev sources.
+macro(dmitigr_add_libev libev_src_root)
+  set(dmitigr_ev_headers
+    ${libev_src_root}/ev.h
+    ${libev_src_root}/ev++.h
+    ${libev_src_root}/event.h)
+  set(dmitigr_ev_implementations
+    ${libev_src_root}/ev.c
+    ${libev_src_root}/event.c)
 
-set(ev_headers ev.h ev++.h event.h)
-set(ev_implementations ev.c event.c)
+  add_library(ev STATIC
+    ${dmitigr_ev_headers}
+    ${dmitigr_ev_implementations})
 
-add_library(ev STATIC ${ev_headers} ${ev_implementations})
+  if(LIBEV_INSTALL)
+    install(TARGETS ev
+      ARCHIVE DESTINATION ${CMAKE_INSTALL_PREFIX}
+      LIBRARY DESTINATION ${CMAKE_INSTALL_PREFIX}
+      RUNTIME DESTINATION ${CMAKE_INSTALL_PREFIX})
 
-install(TARGETS ev
-  ARCHIVE  DESTINATION ${CMAKE_INSTALL_PREFIX}
-  LIBRARY  DESTINATION ${CMAKE_INSTALL_PREFIX}
-  RUNTIME  DESTINATION ${CMAKE_INSTALL_PREFIX})
+    install(FILES ${dmitigr_ev_headers}
+      DESTINATION ${CMAKE_INSTALL_PREFIX})
+  endif()
 
-install(FILES ${libev_headers}
-  DESTINATION ${CMAKE_INSTALL_PREFIX})
+  foreach(suff headers implementations)
+    unset(dmitigr_ev_${suff})
+  endforeach()
+endmacro()
--- src/dmitigr_sqlite3.cmake
+++ src/dmitigr_sqlite3.cmake
@@ -1,42 +1,50 @@
 # -*- cmake -*-
 # Copyright (C) Dmitry Igrishin
 
-project(sqlite3)
-cmake_minimum_required(VERSION 3.16)
+# @brief Adds SQLite 3 with add_library() to the project.
+#
+# @details The output of this macro depends on the following variables:
+#   - `SQLITE3_SHELL` - Boolean which indicates the request to build SQLite
+#   shell;
+#   - `SQLITE3_INSTALL` - Boolean which indicates the request to install.
+#
+# @param sqlite3_src_root A path where to look for SQLite sources.
+macro(dmitigr_add_sqlite3 sqlite3_src_root)
+  set(dmitigr_sqlite3_headers
+    ${sqlite3_src_root}/sqlite3.h
+    ${sqlite3_src_root}/sqlite3ext.h)
 
-set(SQLITE3_SHELL On CACHE BOOL
-  "Build SQLite shell?")
+  add_library(sqlite3 STATIC
+    ${dmitigr_sqlite3_headers}
+    ${sqlite3_src_root}/sqlite3.c)
 
-set(SQLITE3_INSTALL On CACHE BOOL
-  "Install SQLite?")
+  if(UNIX)
+    target_link_libraries(sqlite3 pthread)
+  endif()
+  if(SQLITE3_INSTALL)
+    target_include_directories(sqlite3 PUBLIC include)
+  else()
+    target_include_directories(sqlite3 PUBLIC ${sqlite3_src_root})
+  endif()
+  target_compile_definitions(sqlite3 PUBLIC
+    SQLITE_OMIT_DEPRECATED
+    SQLITE_OMIT_LOAD_EXTENSION)
+  list(APPEND targets sqlite3)
 
-set(sqlite3_headers sqlite3.h sqlite3ext.h)
+  if(SQLITE3_SHELL)
+    add_executable(sqlite3shell ${sqlite3_src_root}/shell.c)
+    target_link_libraries(sqlite3shell sqlite3)
+    list(APPEND targets sqlite3shell)
+  endif()
 
-add_library(sqlite3 STATIC ${sqlite3_headers} sqlite3.c)
-if (UNIX)
-  target_link_libraries(sqlite3 pthread)
-endif()
-if(SQLITE3_INSTALL)
-  target_include_directories(sqlite3 PUBLIC include)
-else()
-  target_include_directories(sqlite3 PUBLIC ${CMAKE_CURRENT_SOURCE_DIR})
-endif()
-target_compile_definitions(sqlite3 PUBLIC
-  SQLITE_OMIT_DEPRECATED
-  SQLITE_OMIT_LOAD_EXTENSION)
-list(APPEND targets sqlite3)
+  if(SQLITE3_INSTALL)
+    install(TARGETS ${targets}
+      ARCHIVE DESTINATION lib
+      LIBRARY DESTINATION lib
+      RUNTIME DESTINATION bin)
 
-if(SQLITE3_SHELL)
-  add_executable(sqlite3shell shell.c)
-  target_link_libraries(sqlite3shell sqlite3)
-  list(APPEND targets sqlite3shell)
-endif()
+    install(FILES ${dmitigr_sqlite3_headers} DESTINATION include)
+  endif()
 
-if(SQLITE3_INSTALL)
-  install(TARGETS ${targets}
-    ARCHIVE DESTINATION lib
-    LIBRARY DESTINATION lib
-    RUNTIME DESTINATION bin)
-
-  install(FILES ${sqlite3_headers} DESTINATION include)
-endif()
+  unset(dmitigr_sqlite3_headers)
+endmacro()
